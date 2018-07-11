b4093fdbf966c90b218650d7e090f04f50fdbf0e
Nasser Abouelazm <nasser.abouelazm@gmail.com>
2018-07-10 21:57:16 -0400

    remove no-cors

--- src/utils/EarthFactory.js
+++ src/utils/EarthFactory.js
@@ -3,9 +3,7 @@ const baseUrl = 'https://api.nasa.gov/planetary/earth/imagery?'
 
 export default class EarthFactory {
 	static fetchImage(lat, lng) {
-		return fetch(baseUrl + `lon=${lng}&lat=${lat}&api_key=${localKeys.NASA_API_KEY}`, {
-            mode: 'no-cors'
-        })
+		return fetch(baseUrl + `lon=${lng}&lat=${lat}&api_key=${localKeys.NASA_API_KEY}`)
 				.then(res=>res.json())
 	}
 }
