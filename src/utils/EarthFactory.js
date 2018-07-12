47fbe576ab569d5d55a945fef8780018557c2b3d
nasser85 <nasser.abouelazm@gmail.com>
2018-07-12 11:52:23 -0400

    update fetch to jquery getJSON

--- src/utils/EarthFactory.js
+++ src/utils/EarthFactory.js
@@ -1,9 +1,10 @@
 import { localKeys } from '../config/local.keys.js'
+import $ from 'jquery'
 const baseUrl = 'https://api.nasa.gov/planetary/earth/imagery?'
 
 export default class EarthFactory {
 	static fetchImage(lat, lng) {
-		return fetch(baseUrl + `lon=${lng}&lat=${lat}&api_key=${localKeys.NASA_API_KEY}`)
-				.then(res=>res.json())
+		return $.getJSON(baseUrl + `lon=${lng}&lat=${lat}&api_key=${localKeys.NASA_API_KEY}`)
+				.then(res=>res)
 	}
 }
